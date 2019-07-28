b0d9ac78befb8b7e8f47df5f4a2fdb2ea6ed0415
akhileshsh026 <akhileshsh026@hotmail.com>
2019-07-28 12:04:30 +0530

    the constructorin the App has another way to set the state.

--- Basics-1.js
+++ Basics-1.js
@@ -1,22 +1,73 @@
-const render = () => {
-  document.getElementById("mountNode").innerHTML = `
-     <Div>
-         Hello HTML
-       <input />
-       <pre> ${new Date().toLocaleTimeString()} </pre>
-      </Div>
-  `;
+class Card extends React.Component {
+  render() {
+    const profile = this.props;
+    return (
+      <div style={{ margin: "1rem" }}>
+        <img src={profile.avatar_url} />
+        <div classname="info">
+          <div className="name"> {profile.name} </div>
+          <div className="compnay"> Compnay here ... {profile.company} </div>
+        </div>
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(
-    React.createElement(
-      "div",
-      null,
-      "Hello react",
-      React.createElement("input", null),
-      React.createElement("pre", null, new Date().toLocaleTimeString())
-    ),
-    document.getElementById("mountNode2")
-  );
-};
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook"
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu"
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook"
+  }
+];
 
-setInterval(render, 1000);
+const CardList = props => (
+  <div>
+    {props.profiles.map(profile => (
+      <Card {...profile} />
+    ))}
+    />
+  </div>
+);
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" placeholder="Github response" />
+        <button> Add Card </button>
+      </form>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      profiles: testData
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header"> {this.props.title} </div>
+        <Form />
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App title="The GitHub Cards App" />, mountNode);
